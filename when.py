bee4185105d3f15d6a222655a69fadfc24d1f087
Chuanxiu Xiong <chuanxiuxiong@outlook.com>
2020-04-11 20:15:46 -0700

    main

--- when.py
+++ when.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import logging
 import os
@@ -10,30 +11,24 @@ from src.login import login
 from src.action import checkAvailability, checkout
 
 
-def main():
-    # load env variables (including username and password for login)
-    load_dotenv()
+# load env variables (including username and password for login)
+load_dotenv()
 
-    logging.basicConfig(filename=os.getenv(
-        'LOG_FILENAME'), level=logging.DEBUG, filemode='w')
+logging.basicConfig(filename=os.getenv(
+    'LOG_FILENAME'), level=logging.DEBUG, filemode='w')
 
-    session = requests.Session()
+session = requests.Session()
 
-    # login
-    response = login(logging, session, os.getenv(
-        'EMAIL'), os.getenv('PASSWORD'))
+# login
+login(logging, session, os.getenv('EMAIL'), os.getenv('PASSWORD'))
 
-    while True:
-        # check availability
-        isAvailable = checkAvailability(session)
-        if isAvailable:
-            # checkout
-            checkout(session)
-            return
+while True:
+    # check availability
+    isAvailable = checkAvailability(session)
+    if isAvailable > 0:
+        # checkout
+        checkout(session)
+        exit()
 
-        # sleep this thread while allowing others
-        time.sleep(random.randint(3, 6))
-
-
-if __name__ == '__main__':
-    main()
+    # sleep this thread while allowing others
+    time.sleep(random.randint(3, 6))
